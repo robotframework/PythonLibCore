45e7b7503e5aa9afdc32280768858604cb4d7346
Tatu Aalto <aalto.tatu@gmail.com>
2020-03-07 00:11:24 +0200

    Coverage report

--- utest/run.py
+++ utest/run.py
@@ -10,12 +10,14 @@ curdir = dirname(abspath(__file__))
 atest_dir = join(curdir, '..', 'atest')
 python_version = platform.python_version()
 xunit_report = join(atest_dir, 'results', 'xunit-%s.xml' % python_version)
-sys.path.insert(0, join(curdir, '..', 'src'))
+src = join(curdir, '..', 'src')
+sys.path.insert(0, src)
 sys.path.insert(0, atest_dir)
 pytest_args = sys.argv[1:] + [
     '-p', 'no:cacheprovider',
     '--junitxml=%s' % xunit_report,
     '-o', 'junit_family=xunit2',
+    '--cov=%s' % src,
     curdir
 ]
 rc = pytest.main(pytest_args)
