4223697ba16810ed4475d14d148bec1b447942c0
Tatu Aalto <aalto.tatu@gmail.com>
2020-03-14 18:53:26 +0200

    RF version to output files

--- utest/run.py
+++ utest/run.py
@@ -4,12 +4,12 @@ from os.path import abspath, dirname, join
 import sys
 
 import pytest
-
+from robot.version import VERSION as rf_version
 
 curdir = dirname(abspath(__file__))
 atest_dir = join(curdir, '..', 'atest')
 python_version = platform.python_version()
-xunit_report = join(atest_dir, 'results', 'xunit-%s.xml' % python_version)
+xunit_report = join(atest_dir, 'results', 'xunit-python-%s-robot%s.xml' % (python_version, rf_version))
 src = join(curdir, '..', 'src')
 sys.path.insert(0, src)
 sys.path.insert(0, atest_dir)
