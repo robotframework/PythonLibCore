2ed3564bd5b4b5c5430bbd436b447ae8b934b3a0
Tatu Aalto <aalto.tatu@gmail.com>
2020-04-26 21:17:43 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -25,21 +25,24 @@ Framework :: Robot Framework
 with open(join(CURDIR, 'src', 'robotlibcore.py')) as f:
     VERSION = re.search("\n__version__ = '(.*)'", f.read()).group(1)
 with open(join(CURDIR, 'README.rst')) as f:
-    DESCRIPTION = f.read()
+    LONG_DESCRIPTION = f.read()
 
+DESCRIPTION = ('Tools to ease creating larger test libraries for '
+               'Robot Framework using Python.')
 setup(
-    name            = 'robotframework-pythontlibcore',
-    version         = VERSION,
-    author          = 'Tatu Aalto',
-    author_email    = 'aalto.tatu@gmail.com',
-    url             = 'https://github.com/robotframework/PythonLibCore',
-    license         = 'Apache License 2.0',
-    description     = DESCRIPTION,
-    keywords        = 'robotframework testing testautomation library development',
-    platforms       = 'any',
-    classifiers     = CLASSIFIERS,
-    python_requires = '>=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
-    package_dir     = {'': 'src'},
-    packages        = find_packages('src'),
-    py_modules      = ['robotlibcore'],
+    name             = 'robotframework-pythontlibcore',
+    version          = VERSION,
+    author           = 'Tatu Aalto',
+    author_email     = 'aalto.tatu@gmail.com',
+    url              = 'https://github.com/robotframework/PythonLibCore',
+    license          = 'Apache License 2.0',
+    description      = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    keywords         = 'robotframework testing testautomation library development',
+    platforms        = 'any',
+    classifiers      = CLASSIFIERS,
+    python_requires  = '>=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
+    package_dir      = {'': 'src'},
+    packages         = find_packages('src'),
+    py_modules       = ['robotlibcore'],
 )
