4def58d99f0c0e64538ede834053eb48cd063729
Tatu Aalto <aalto.tatu@gmail.com>
2020-04-26 21:20:03 +0300

    Fixed setup.py again

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Programming Language :: Python :: 2
-'Programming Language :: Python :: 2.7
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
