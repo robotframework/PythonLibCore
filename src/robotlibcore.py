2515b2f6cc637184744db5b5371af5580e8363df
Tatu Aalto <aalto.tatu@gmail.com>
2020-07-10 00:03:55 +0300

    Back to dev version

--- src/robotlibcore.py
+++ src/robotlibcore.py
@@ -36,7 +36,7 @@ from robot import __version__ as robot_version
 PY2 = sys.version_info < (3,)
 RF31 = robot_version < '3.2'
 
-__version__ = '2.1.0'
+__version__ = '2.1.1.dev1'
 
 
 class HybridCore(object):
