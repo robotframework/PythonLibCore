c4cfd415eef4640fde6771ba264c2bb8e8c8f536
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-01-05 11:50:17 +0200

    Refactor moc_library_py3 into moc_library

--- atest/moc_library.py
+++ atest/moc_library.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from robot.api.deco import keyword
 
 
@@ -26,4 +28,19 @@ class MockLibrary:
         pass
 
     def varargs_kwargs(self, *vargs, **kwargs):
+        pass
+
+    def named_only(self, *varargs, key1, key2):
+        pass
+
+    def named_only_with_defaults(self, *varargs, key1, key2, key3='default1', key4=True):
+        pass
+
+    def args_with_type_hints(self, arg1, arg2, arg3: str, arg4: None) -> bool:
+        pass
+
+    def self_and_keyword_only_types(x: 'MockLibrary', mandatory, *varargs: int, other: bool, **kwargs: int):
+        pass
+
+    def optional_none(self, xxx, arg1: Optional[str] = None, arg2: Optional[str] = None, arg3=False):
         pass
