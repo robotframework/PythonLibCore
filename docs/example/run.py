ab3ba40ddde60cdf67ed3b90036a8f0f33af83fc
Tatu Aalto <aalto.tatu@gmail.com>
2021-02-07 01:18:30 +0200

    Inprove runner

--- docs/example/run.py
+++ docs/example/run.py
@@ -5,10 +5,14 @@ from robot import run_cli
 parser = argparse.ArgumentParser("Runner for examples")
 parser.add_argument("type", help="Which example is run.")
 args = parser.parse_args()
-if args.type not in ["static", "dynamic"]:
+if args.type == "static":
+    folder = f"01-{args.type}"
+elif args.type == "hybrid":
+    folder = f"02-{args.type}"
+else:
     raise ValueError("Invalid value for library type.")
 run_cli([
     "--pythonpath",
-    args.type,
-    args.type
+    folder,
+    folder
 ])
