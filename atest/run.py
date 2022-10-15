cd6b0f21a6a9270e86fede084312cd591a948a31
Tatu Aalto <aalto.tatu@gmail.com>
2022-10-16 00:24:43 +0300

    Runner improvements

--- atest/run.py
+++ atest/run.py
@@ -1,42 +1,65 @@
 #!/usr/bin/env python
-
-
 import platform
-from os.path import abspath, dirname, join
+import shutil
 import sys
+from os.path import abspath, dirname, isdir, join
+from pathlib import Path
 
-from robot import run, rebot
+from robot import rebot, run
 from robot.version import VERSION as rf_version
 from robotstatuschecker import process_output
 
-
-library_variants = ['Hybrid', 'Dynamic', 'ExtendExisting']
+library_variants = ["Hybrid", "Dynamic", "ExtendExisting"]
 curdir = dirname(abspath(__file__))
-outdir = join(curdir, 'results')
-tests = join(curdir, 'tests.robot')
-tests_types = join(curdir, 'tests_types.robot')
-sys.path.insert(0, join(curdir, '..', 'src'))
+outdir = join(curdir, "results")
+if isdir(outdir):
+    shutil.rmtree(outdir, ignore_errors=True)
+tests = join(curdir, "tests.robot")
+tests_types = join(curdir, "tests_types.robot")
+plugin_api = join(curdir, "plugin_api")
+sys.path.insert(0, join(curdir, "..", "src"))
 python_version = platform.python_version()
 for variant in library_variants:
-    output = join(outdir, 'lib-{}-python-{}-robot-{}.xml'.format(variant, python_version, rf_version))
-    rc = run(tests, name=variant, variable='LIBRARY:%sLibrary' % variant,
-             output=output, report=None, log=None, loglevel='debug')
+    output = join(outdir, "lib-{}-python-{}-robot-{}.xml".format(variant, python_version, rf_version))
+    rc = run(
+        tests,
+        name=variant,
+        variable="LIBRARY:%sLibrary" % variant,
+        output=output,
+        report=None,
+        log=None,
+        loglevel="debug",
+    )
     if rc > 250:
         sys.exit(rc)
     process_output(output, verbose=False)
-output = join(outdir, 'lib-DynamicTypesLibrary-python-{}-robot-{}.xml'.format(python_version, rf_version))
+output = join(outdir, "lib-DynamicTypesLibrary-python-{}-robot-{}.xml".format(python_version, rf_version))
 exclude = "py310" if sys.version_info < (3, 10) else ""
-rc = run(tests_types, name='Types', output=output, report=None, log=None, loglevel='debug', exclude=exclude)
+rc = run(tests_types, name="Types", output=output, report=None, log=None, loglevel="debug", exclude=exclude)
 if rc > 250:
     sys.exit(rc)
 process_output(output, verbose=False)
-print('\nCombining results.')
-library_variants.append('DynamicTypesLibrary')
-rc = rebot(*(join(outdir, 'lib-{}-python-{}-robot-{}.xml'.format(variant, python_version, rf_version)) for variant in library_variants),
-           **dict(name='Acceptance Tests', outputdir=outdir, log='log-python-{}-robot-{}.html'.format(python_version, rf_version),
-                  report='report-python-{}-robot-{}.html'.format(python_version, rf_version)))
+output = join(outdir, "lib-PluginApi-python-{}-robot-{}.xml".format(python_version, rf_version))
+rc = run(plugin_api, name="Plugin", output=output, report=None, log=None, loglevel="debug")
+if rc > 250:
+    sys.exit(rc)
+process_output(output, verbose=False)
+print("\nCombining results.")
+library_variants.append("DynamicTypesLibrary")
+xml_files = [str(xml_file) for xml_file in Path(outdir).glob("*.xml")]
+for xxx in xml_files:
+    print(xxx)
+rc = rebot(
+    *xml_files,
+    **dict(
+        name="Acceptance Tests",
+        outputdir=outdir,
+        log="log-python-{}-robot-{}.html".format(python_version, rf_version),
+        report="report-python-{}-robot-{}.html".format(python_version, rf_version),
+    ),
+)
 if rc == 0:
-    print('\nAll tests passed/failed as expected.')
+    print("\nAll tests passed/failed as expected.")
 else:
-    print('\n%d test%s failed.' % (rc, 's' if rc != 1 else ''))
+    print("\n%d test%s failed." % (rc, "s" if rc != 1 else ""))
 sys.exit(rc)
--- utest/run.py
+++ utest/run.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 import argparse
 import platform
-from os.path import abspath, dirname, join
 import sys
+from os.path import abspath, dirname, join
 
 import pytest
 from robot.version import VERSION as rf_version
 
 curdir = dirname(abspath(__file__))
-atest_dir = join(curdir, '..', 'atest')
+atest_dir = join(curdir, "..", "atest")
 python_version = platform.python_version()
-xunit_report = join(atest_dir, 'results', 'xunit-python-{}-robot{}.xml'.format(python_version, rf_version))
-src = join(curdir, '..', 'src')
+xunit_report = join(atest_dir, "results", "xunit-python-{}-robot{}.xml".format(python_version, rf_version))
+src = join(curdir, "..", "src")
 sys.path.insert(0, src)
 sys.path.insert(0, atest_dir)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--no-cov', dest='cov', action='store_false')
-parser.add_argument('--cov', dest='cov', action='store_true')
+parser.add_argument("--no-cov", dest="cov", action="store_false")
+parser.add_argument("--cov", dest="cov", action="store_true")
 parser.set_defaults(cov=True)
 args = parser.parse_args()
 
 pytest_args = [
-    '-p', 'no:cacheprovider',
-    '--junitxml=%s' % xunit_report,
-    '-o', 'junit_family=xunit2',
-    '--showlocals',
-    curdir
+    "-p",
+    "no:cacheprovider",
+    "--junitxml=%s" % xunit_report,
+    "-o",
+    "junit_family=xunit2",
+    "--showlocals",
+    curdir,
 ]
 if args.cov:
-    pytest_args.insert(0, '--cov=%s' % src)
+    pytest_args.insert(0, "--cov=%s" % src)
 rc = pytest.main(pytest_args)
 sys.exit(rc)
