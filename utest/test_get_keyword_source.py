65412f370854082be71391e90bbbc1b1df85299c
Tatu Aalto <aalto.tatu@gmail.com>
2022-10-16 01:01:09 +0300

    Black for utests

--- utest/test_get_keyword_source.py
+++ utest/test_get_keyword_source.py
@@ -8,87 +8,87 @@ from DynamicLibrary import DynamicLibrary
 from DynamicTypesLibrary import DynamicTypesLibrary
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def lib():
     return DynamicLibrary()
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def lib_types():
     return DynamicTypesLibrary()
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def cur_dir():
     return path.dirname(__file__)
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def lib_path(cur_dir):
-    return path.normpath(path.join(cur_dir, '..', 'atest', 'DynamicLibrary.py'))
+    return path.normpath(path.join(cur_dir, "..", "atest", "DynamicLibrary.py"))
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def lib_path_components(cur_dir):
-    return path.normpath(path.join(cur_dir, '..', 'atest', 'librarycomponents.py'))
+    return path.normpath(path.join(cur_dir, "..", "atest", "librarycomponents.py"))
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def lib_path_types(cur_dir):
-    return path.normpath(path.join(cur_dir, '..', 'atest', 'DynamicTypesLibrary.py'))
+    return path.normpath(path.join(cur_dir, "..", "atest", "DynamicTypesLibrary.py"))
 
 
 def test_location_in_main(lib, lib_path):
-    source = lib.get_keyword_source('keyword_in_main')
-    assert source == '%s:20' % lib_path
+    source = lib.get_keyword_source("keyword_in_main")
+    assert source == "%s:20" % lib_path
 
 
 def test_location_in_class(lib, lib_path_components):
-    source = lib.get_keyword_source('method')
-    assert source == '%s:13' % lib_path_components
+    source = lib.get_keyword_source("method")
+    assert source == "%s:13" % lib_path_components
 
 
 def test_decorator_wrapper(lib_types, lib_path_types):
-    source = lib_types.get_keyword_source('keyword_wrapped')
-    assert source == '%s:74' % lib_path_types
+    source = lib_types.get_keyword_source("keyword_wrapped")
+    assert source == "%s:74" % lib_path_types
 
 
 def test_location_in_class_custom_keyword_name(lib, lib_path_components):
-    source = lib.get_keyword_source('Custom name')
-    assert source == '%s:17' % lib_path_components
+    source = lib.get_keyword_source("Custom name")
+    assert source == "%s:17" % lib_path_components
 
 
 def test_no_line_number(lib, lib_path, when):
     when(lib)._DynamicCore__get_keyword_line(Any()).thenReturn(None)
-    source = lib.get_keyword_source('keyword_in_main')
+    source = lib.get_keyword_source("keyword_in_main")
     assert source == lib_path
 
 
 def test_no_path(lib, when):
     when(lib)._DynamicCore__get_keyword_path(Any()).thenReturn(None)
-    source = lib.get_keyword_source('keyword_in_main')
-    assert source == ':20'
+    source = lib.get_keyword_source("keyword_in_main")
+    assert source == ":20"
 
 
 def test_no_path_and_no_line_number(lib, when):
     when(lib)._DynamicCore__get_keyword_path(Any()).thenReturn(None)
     when(lib)._DynamicCore__get_keyword_line(Any()).thenReturn(None)
-    source = lib.get_keyword_source('keyword_in_main')
+    source = lib.get_keyword_source("keyword_in_main")
     assert source is None
 
 
 def test_def_in_decorator(lib_types, lib_path_types):
-    source = lib_types.get_keyword_source('keyword_with_def_deco')
-    assert source == '%s:68' % lib_path_types
+    source = lib_types.get_keyword_source("keyword_with_def_deco")
+    assert source == "%s:68" % lib_path_types
 
 
 def test_error_in_getfile(lib, when):
-    when(inspect).getfile(Any()).thenRaise(TypeError('Some message'))
-    source = lib.get_keyword_source('keyword_in_main')
+    when(inspect).getfile(Any()).thenRaise(TypeError("Some message"))
+    source = lib.get_keyword_source("keyword_in_main")
     assert source is None
 
 
 def test_error_in_line_number(lib, when, lib_path):
-    when(inspect).getsourcelines(Any()).thenRaise(IOError('Some message'))
-    source = lib.get_keyword_source('keyword_in_main')
+    when(inspect).getsourcelines(Any()).thenRaise(IOError("Some message"))
+    source = lib.get_keyword_source("keyword_in_main")
     assert source == lib_path
