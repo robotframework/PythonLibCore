272efd926efd47f9477bfa2790ab2975c13c6388
Tatu Aalto <aalto.tatu@gmail.com>
2022-10-16 00:42:55 +0300

    Delete unsued static core

--- docs/example/run.py
+++ docs/example/run.py
@@ -5,9 +5,7 @@ from robot import run_cli
 parser = argparse.ArgumentParser("Runner for examples")
 parser.add_argument("type", help="Which example is run.")
 args = parser.parse_args()
-if args.type == "static":
-    folder = f"01-{args.type}"
-elif args.type == "hybrid":
+if args.type == "hybrid":
     folder = f"02-{args.type}"
 else:
     raise ValueError("Invalid value for library type.")
